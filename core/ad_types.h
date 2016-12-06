e1c8e568e52ab674afdc7fd9f82c32072c96a96d
Veeraj S Khokale <veerajsk.work@gmail.com>
2016-12-06 21:01:56 +0530

    Usability and formatting changes to the Unit test framework
    1. Add macros to ease the definition of unit test cases
    2. Make formatting improvements to the unit test case execution output
    3. Format the source code to maintain project complaince
    4. Add the double type to ad_types.h
    5. Add a virtual destructor to the Unit_test class

--- core/ad_types.h
+++ core/ad_types.h
@@ -13,14 +13,14 @@ namespace ad
 #define AD_MAGENTA          "\033[35m"      
 #define AD_CYAN             "\033[36m"      
 #define AD_WHITE            "\033[37m"      
-#define AD_BOLDBLACK        "\033[1m\033[30m"      
-#define AD_BOLDRED          "\033[1m\033[31m"      
-#define AD_BOLDGREEN        "\033[1m\033[32m"      
-#define AD_BOLDYELLOW       "\033[1m\033[33m"      
-#define AD_BOLDBLUE         "\033[1m\033[34m"      
-#define AD_BOLDMAGENTA      "\033[1m\033[35m"      
-#define AD_BOLDCYAN         "\033[1m\033[36m"      
-#define AD_BOLDWHITE        "\033[1m\033[37m"
+#define AD_BOLD_BLACK       "\033[1m\033[30m"      
+#define AD_BOLD_RED         "\033[1m\033[31m"      
+#define AD_BOLD_GREEN       "\033[1m\033[32m"      
+#define AD_BOLD_YELLOW      "\033[1m\033[33m"      
+#define AD_BOLD_BLUE        "\033[1m\033[34m"      
+#define AD_BOLD_MAGENTA     "\033[1m\033[35m"      
+#define AD_BOLD_CYAN        "\033[1m\033[36m"      
+#define AD_BOLD_WHITE       "\033[1m\033[37m"
 
 using Void      = void;
 using Bool      = bool;
@@ -32,15 +32,17 @@ using Int16     = int16_t;
 using Int32     = int32_t;
 using Int64     = int64_t;
 
-using Uchar      = unsigned char;
-using Ubyte      = uint8_t;
-using Uint8      = uint8_t;
-using Uint16     = uint16_t;
-using Uint32     = uint32_t;
-using Uint64     = uint64_t;
+using Uchar     = unsigned char;
+using Ubyte     = uint8_t;
+using Uint8     = uint8_t;
+using Uint16    = uint16_t;
+using Uint32    = uint32_t;
+using Uint64    = uint64_t;
 
-using Int        = Int32;
-using Uint       = Uint32;
+using Int       = Int32;
+using Uint      = Uint32;
+
+using Double    = double;
 
 }
 
--- test/ad_ut.h
+++ test/ad_ut.h
@@ -16,6 +16,9 @@
 namespace ad
 {
 
+namespace internal
+{
+
 class Assert_error
     : public std::exception
 {
@@ -41,9 +44,15 @@ public:
         return m_exp.c_str();
     }
 
-    std::string get_exp() const noexcept { return m_exp; }
+    std::string get_exp() const noexcept 
+    { 
+        return m_exp; 
+    }
 
-    Bool has_func() const noexcept { return static_cast<Bool>(m_func); }
+    Bool has_func() const noexcept 
+    { 
+        return static_cast<Bool>(m_func); 
+    }
     
     void call_func(std::ostringstream& strm) const noexcept 
     {
@@ -74,17 +83,50 @@ public:
 
     virtual void operator()() = 0;
 
+    virtual ~Unit_test() noexcept
+    {
+    }
+
 private:
-    std::string get_name() const noexcept { return m_name; }
-    std::string get_file() const  noexcept { return m_file; }
-    Int get_line() const noexcept { return m_line; }
+    std::string get_name() const noexcept 
+    { 
+        return m_name; 
+    }
+    
+    std::string get_file() const  noexcept 
+    { 
+        return m_file; 
+    }
 
-    Bool is_executed() const noexcept { return m_exec; }
-    Time_point get_start() const noexcept { return m_start; }
-    Time_point get_end() const noexcept { return m_end; }
+    Int get_line() const noexcept 
+    { 
+        return m_line; 
+    }
 
-    Bool is_failed() const noexcept { return m_fail; }
-    std::string get_info() const noexcept { return m_strm.str(); }
+    Bool is_executed() const noexcept 
+    { 
+        return m_exec; 
+    }
+
+    Time_point get_start() const noexcept 
+    { 
+        return m_start; 
+    }
+    
+    Time_point get_end() const noexcept 
+    { 
+        return m_end; 
+    }
+
+    Bool is_failed() const noexcept 
+    { 
+        return m_fail; 
+    }
+    
+    std::string get_info() const noexcept 
+    { 
+        return m_strm.str(); 
+    }
 
     void initialize() noexcept
     {
@@ -153,31 +195,7 @@ private:
     Bool                        m_fail;
     std::ostringstream          m_strm;
 
-};
-
-#define AD_UT_REGISTER(ut) \
-public: \
-    ut() \
-        : Unit_test(#ut, __FILE__, __LINE__) \
-    { \
-    }
-
-#define AD_UT_ASSERT1(exp) do { \
-    if (!(exp)) \
-    { \
-        throw ad::Assert_error(#exp); \
-    } \
-} while (0)
-
-#define AD_UT_ASSERT2(exp, func) do { \
-    if (!(exp)) \
-    { \
-        throw ad::Assert_error(#exp, func); \
-    } \
-} while (0)
-
-#define AD_GET_MACRO(_0, _1, _2, NAME, ...) NAME
-#define AD_UT_ASSERT(...) AD_GET_MACRO(_0, __VA_ARGS__, AD_UT_ASSERT2, AD_UT_ASSERT1)(__VA_ARGS__)      
+};      
 
 using Ut_runner = std::vector<std::unique_ptr<Unit_test>>;
 
@@ -187,63 +205,68 @@ void ut_add(Ut_runner& ut_runner)
     ut_runner.push_back(std::make_unique<Ut>());
 }
 
-template <class Forward_it, class Stream>
-void ut_run(Forward_it first, Forward_it last, Stream& strm)
+template <class Forward_it, class Out_stream>
+void ut_run(Forward_it first, Forward_it last, Out_stream& strm)
 {
-    const Int SLNO_WIDTH = 8;
-    const Int NAME_WIDTH = 20;
-    const Int PASS_WIDTH = 12;
-    const Int TIME_WIDTH = 12;
-    const Int FULL_WIDTH = SLNO_WIDTH + NAME_WIDTH + PASS_WIDTH + TIME_WIDTH;
-
-    strm << std::left << "\n\n" <<
-        std::setw(SLNO_WIDTH) << "SLNO" <<
-        std::setw(NAME_WIDTH) << "NAME" <<
-        std::setw(PASS_WIDTH) << "PASS/FAIL" <<
-        std::setw(TIME_WIDTH) << "TIME(s)" << '\n';
-
-    for (Int i = 0; i < FULL_WIDTH; ++i)
-    {
-        strm << '-';
-    }
-    strm << "\n\n";
-
     Int cnt = 0, fcnt = 0;
+    Double tot_time = 0;
+
+    strm << "\n\n";
+    
     for (auto ut = first; ut != last; ++ut)
     {
         ++cnt;
         (*ut)->run();
-        std::chrono::duration<double> duration = (*ut)->get_end() - (*ut)->get_start();
+        std::chrono::duration<Double> duration = (*ut)->get_end() - (*ut)->get_start();
+        tot_time += duration.count();
 
-        strm << std::left <<
-            std::setw(SLNO_WIDTH) << cnt <<
-            std::setw(NAME_WIDTH) << (*ut)->get_name();
+        strm << AD_BLUE << "[RUN] " << AD_RESET << (*ut)->get_name();
 
         if ((*ut)->is_failed())
         {
             ++fcnt;
-            strm << std::left <<
-                AD_RED << std::setw(PASS_WIDTH) << "FAIL" << AD_RESET;
+            strm << AD_RED;
+            
+            Int length = (*ut)->get_name().size();
+            Int num = (25 - length) > 0 ? (25 - length) : 0;
+            
+            strm << ' ';
+            for (Int i = 0; i < num; ++i)
+            {
+                strm << '.';
+            }
+            strm << " FAIL" << AD_RESET;
         }
         else
         {
-            strm << std::left <<
-                AD_GREEN << std::setw(PASS_WIDTH) << "PASS" << AD_RESET;
+            strm << AD_GREEN;
+
+            Int length = (*ut)->get_name().size();
+            Int num = (25 - length) > 0 ? (25 - length) : 0;
+            
+            strm << ' ';
+            for (Int i = 0; i < num; ++i)
+            {
+                strm << '.';
+            }
+            strm << " PASS" << AD_RESET;
         }
 
-        strm << std::left <<
-            std::setw(TIME_WIDTH) << 
-            duration.count() << '\n';
+        strm << std::setw(30) << std::right <<
+            " [" << std::setw(10) << std::right << duration.count() << "s] \n";
     }
-    strm << "\n\nUnit Tests failed: " << fcnt << "\n\n";
+    strm << 
+        "\nTotal  : " << cnt <<
+        "\nFailed : " << fcnt <<
+        "\nTime   : " << tot_time << "s\n\n";
+
+    strm << AD_RED << "FAILED UNIT TESTS\n-----------------\n";
 
     for (auto ut = first; ut != last; ++ut)
     {
         if ((*ut)->is_failed())
         {
-            strm <<
-                "DETAILS\n" << "-------\n" << 
-                "NAME  : " << (*ut)->get_name() << '\n' <<
+            strm << AD_BLUE << "[" << (*ut)->get_name() << "] \n" << AD_RESET <<
                 "FILE  : " << (*ut)->get_file() << '\n' <<
                 "LINE  : " << (*ut)->get_line() << '\n' <<
                 (*ut)->get_info() << '\n' << std::endl;
@@ -252,5 +275,45 @@ void ut_run(Forward_it first, Forward_it last, Stream& strm)
 }
 
 }
+
+}
+
+#define AD_UT(ut) \
+class ut \
+    : public ad::internal::Unit_test \
+{ \
+public: \
+    ut() \
+        : ad::internal::Unit_test(#ut, __FILE__, __LINE__) \
+    { \
+    } \
+    \
+    void operator()(); \
+}; \
+\
+void ut::operator()() \
+
+#define AD_UT_ASSERT1(exp) do { \
+    if (!(exp)) \
+    { \
+        throw ad::internal::Assert_error(#exp); \
+    } \
+} while (0)
+
+#define AD_UT_ASSERT2(exp, func) do { \
+    if (!(exp)) \
+    { \
+        throw ad::internal::Assert_error(#exp, func); \
+    } \
+} while (0)
+
+#define AD_GET_MACRO(_0, _1, _2, NAME, ...) NAME
+#define AD_UT_ASSERT(...) AD_GET_MACRO(_0, __VA_ARGS__, AD_UT_ASSERT2, AD_UT_ASSERT1)(__VA_ARGS__)
+
+#define AD_UT_RUNNER(ut_runner) ad::internal::Ut_runner ut_runner
+
+#define AD_UT_ADD(ut_runner, ut) ad::internal::ut_add<ut>(ut_runner)
+
+#define AD_UT_RUN(ut_runner, strm) ad::internal::ut_run(ut_runner.begin(), ut_runner.end(), strm)
 
 #endif
