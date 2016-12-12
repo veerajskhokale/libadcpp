102e560b1caead5b5b2caec68b256569c6adf72a
Veeraj S Khokale <veerajsk.work@gmail.com>
2016-12-12 21:07:18 +0530

    Add Utilities file
    Define a generic utility function to create
    objects, which can be used for object creation
    in generic algorithms.

---
+++ core/ad_util.h
@@ -0,0 +1,20 @@
+#ifndef AD_CORE_UTIL_H_
+#define AD_CORE_UTIL_H_
+
+#include <utility>
+#include <type_traits>
+
+#include "ad_types.h"
+
+namespace ad
+{
+
+template <class Object, class... Args>
+std::decay_t<Object> make_object(Args&&... args)
+{
+    return std::decay_t<Object>(std::forward<Args>(args)...);
+}
+
+}
+
+#endif
