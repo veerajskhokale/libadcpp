b59d00e10b754adc49ec47d657f762bede73052c
Veeraj S Khokale <veerajsk.work@gmail.com>
2020-07-06 18:23:50 +0530

    Add helper templates for common constants

--- ad/mp/constant.h
+++ ad/mp/constant.h
@@ -17,6 +17,8 @@
 #ifndef AD_MP_CONSTANT_H_
 #define AD_MP_CONSTANT_H_
 
+#include <ad/types.h>
+
 namespace ad
 {
 namespace mp
@@ -227,6 +229,69 @@ public:
     };
 
 }; /* class Constant_ */
+
+template <Bool val_>
+using Bool_             = Constant_<Bool, val_>;
+
+template <Char val_>
+using Char_             = Constant_<Char, val_>;
+
+template <Byte val_>
+using Byte_             = Constant_<Byte, val_>;
+
+template <Short val_>
+using Short_            = Constant_<Short, val_>;
+
+template <Int val_>
+using Int_              = Constant_<Int, val_>;
+
+template <Long val_>
+using Long_             = Constant_<Long, val_>;
+
+template <Ubyte val_>
+using Ubyte_            = Constant_<Ubyte, val_>;
+
+template <Ushort val_>
+using Ushort_           = Constant_<Ushort, val_>;
+
+template <Uint val_>
+using Uint_             = Constant_<Uint, val_>;
+
+template <Ulong val_>
+using Ulong_            = Constant_<Ulong, val_>;
+
+template <Uint8 val_>
+using Uint8_            = Constant_<Uint8, val_>;
+
+template <Uint16 val_>
+using Uint16_           = Constant_<Uint16, val_>;
+
+template <Uint32 val_>
+using Uint32_           = Constant_<Uint32, val_>;
+
+template <Uint64 val_>
+using Uint64_           = Constant_<Uint64, val_>;
+
+template <Int8 val_>
+using Int8_             = Constant_<Int8, val_>;
+
+template <Int16 val_>
+using Int16_            = Constant_<Int16, val_>;
+
+template <Int32 val_>
+using Int32_            = Constant_<Int32, val_>;
+
+template <Int64 val_>
+using Int64_            = Constant_<Int64, val_>;
+
+template <Size val_>
+using Size_             = Constant_<Size, val_>;
+
+template <PtrDiff val_>
+using PtrDiff_          = Constant_<PtrDiff, val_>;
+
+using True_             = Bool_<1>;
+using False_            = Bool_<0>;
 
 } /* namespace mp */
 } /* namespace ad */
