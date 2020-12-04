0c81374069e86451c79777066d686850eb448f5f
Veeraj S Khokale <veerajsk.work@gmail.com>
2020-12-04 12:42:29 +0530

    clang: fix compile error
    The forEach_ & transform_ meta algorithms used to take a template
    template parameters with 1 & 2 parameters respectively. However this
    meant that templates with variadic parameters could not be used to
    instantiate these algorithms. Therefore these algorithms are modified
    to take a variadic template as their parameter. Note that this was not
    an issue in g++, but clang fails to compile the code.

--- ad/mp/algorithm.h
+++ ad/mp/algorithm.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Veeraj S Khokale All Rights Reserved
+ * Copyright 2020 Veeraj S Khokale All Rights Reserved
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,26 +20,25 @@
 namespace ad {
 namespace mp {
 
-template <class Pack_, template <class> class UnaryFunc_>
+template <class Pack_, template <class...> class UnaryFunc_>
 struct forEach_;
 
 template <
     template <class...> class Pack_, class... Ts_,
-    template <class> class UnaryFunc_>
+    template <class...> class UnaryFunc_>
 struct forEach_<Pack_<Ts_...>, UnaryFunc_> {
     using Result_ = Pack_<typename UnaryFunc_<Ts_>::Result_...>;
 };
 
 template <
     class Pack1_, class Pack2_, template <class...> class OutPack_,
-    template <class, class> class BinaryFunc_>
+    template <class...> class BinaryFunc_>
 struct transform_;
 
 template <
     template <class...> class Pack1_, class... T1s_,
     template <class...> class Pack2_, class... T2s_,
-    template <class...> class OutPack_,
-    template <class, class> class BinaryFunc_>
+    template <class...> class OutPack_, template <class...> class BinaryFunc_>
 struct transform_<Pack1_<T1s_...>, Pack2_<T2s_...>, OutPack_, BinaryFunc_> {
     using Result_ = OutPack_<typename BinaryFunc_<T1s_, T2s_>::Result_...>;
 };
